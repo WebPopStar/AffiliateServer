06da2ec6f8507c67bad8d5dadfff7cd9052fae5c
WebPopStar <aurora980331@gmail.com>
2024-09-07 02:19:00 +0900

    CORS

--- server.js
+++ server.js
@@ -7,19 +7,26 @@ require("dotenv").config();
 
 const app = express();
 app.get("/redirect", async (req, res) => {
-	res.redirect(302, "http://localhost:5173/homepage");
-	// return;
+  res.redirect(302, "http://localhost:5173/homepage");
+  // return;
 });
 
 const port = process.env.PORT || 4000;
 
-// Database connection
+// Databas connection
 DbCon();
 
 // regular middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors()); // allow cross origin requests from client side (browser) to server side
+const allowedOrigins = ["https://*"];
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "OPTIONS"], // Specify allowed methods
+    credentials: true, // Allow credentials if needed
+  })
+); // allow cross origin requests from client side (browser) to server side
 
 // routes
 app.use("/api/user/", UserRoutes);
@@ -30,5 +37,5 @@ app.use(errorHandler);
 
 // listen on port
 const server = app.listen(port, () => {
-	console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
